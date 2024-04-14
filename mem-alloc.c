bcd53f153b8741a3794143593426e671e04620b6
winterrdog <winterrdog@protonmail.ch>
2024-04-14 11:49:33 +0300

    the initiation

---
+++ mem-alloc.c
@@ -0,0 +1,138 @@
+#include "./utils.h"
+
+// NOTE:
+// this program is built with x86-64 in mind. for x86, it might not b as
+// efficient as x86-64. It's also thread-safe
+
+// global singly linked-list trackers
+header_info_t *sl_head = NULL, *sl_tail = NULL;
+
+// global mutex - forget spinlock on this 1
+pthread_mutex_t global_malloc_lock;
+
+void_ptr_t malloc(size_t size)
+{
+	if (!size)
+		return NULL;
+
+	void_ptr_t block = NULL;
+	size_t real_size = 0;
+	header_info_t *header = NULL;
+	pthread_mutex_lock(&global_malloc_lock);
+
+	// is there some already allocate memory?
+	header = get_free_block(size);
+	if (header) {
+		header->is_free = 0;
+		pthread_mutex_unlock(&global_malloc_lock);
+
+		// return the real data address. it's just after the header
+		return (void_ptr_t) (header + 1);
+	}
+	// if there was no allocated memory already, allocate some with 'sbrk'
+	real_size = size + sizeof(header_info_t);
+	block = sbrk(real_size);
+	if (block == (void_ptr_t) - 1) {
+		pthread_mutex_unlock(&global_malloc_lock);
+		return NULL;
+	}
+	// update our global memory linked list
+	header = block;
+	header->is_free = 0, header->size = size, header->next = NULL;
+
+	if (!sl_head)
+		sl_head = header;
+	if (sl_tail)
+		sl_tail->next = header;
+
+	sl_tail = header;
+
+	pthread_mutex_unlock(&global_malloc_lock);
+	return (void_ptr_t) (header + 1);
+}
+
+void_ptr_t calloc(size_t num, size_t nsize)
+{
+	if (num <= 0 || nsize <= 0)
+		return NULL;
+
+	void_ptr_t block = NULL;
+	size_t size = num * nsize;
+
+	// check multiplication overflow
+	if (size % nsize != 0)
+		return NULL;
+
+	block = malloc(size);
+	if (!block)
+		return NULL;
+
+	mm_mem_set(block, 0, size);
+	return block;
+}
+
+void_ptr_t realloc(void *block, size_t size)
+{
+	if (!block || !size)
+		return malloc(size);
+
+	void_ptr_t ret = NULL;
+	header_info_t *header = (header_info_t *) block - 1;
+	if (header->size >= size)
+		return block;
+
+	ret = malloc(size);
+	if (ret) {
+		mm_mem_copy(ret, block, header->size);
+		free(block);
+	}
+	return ret;
+}
+
+void free(void_ptr_t block)
+{
+	if (!block)
+		return;
+
+	void_ptr_t program_break = NULL;
+	header_info_t *header = NULL, *tmp = NULL;
+
+	pthread_mutex_lock(&global_malloc_lock);
+	header = (header_info_t *) block - 1;
+
+	program_break = sbrk(0);	// currrent heap ceiling
+
+	// is the block the last one in the heap
+	if (program_break == ((byte_t *) block + header->size)) {
+		sbrk(0 - (header->size + sizeof(header_info_t)));	// release memory
+
+		// update linked list
+		if (sl_head == sl_tail) {
+			sl_head = sl_tail = NULL;
+			goto clean_up;
+		}
+
+		for (tmp = sl_head; tmp; tmp = tmp->next) {
+			if (tmp->next == sl_tail) {
+				// if we hit the end of list
+				tmp->next = NULL, sl_tail = tmp;
+			}
+		}
+		goto clean_up;
+	}
+	// if the block is "not" the last one in the heap
+	header->is_free = 1;	// mark 'free'
+
+ clean_up:
+	pthread_mutex_unlock(&global_malloc_lock);
+	return;
+}
+
+header_info_t *get_free_block(size_t size)
+{
+	for (header_info_t * curr = sl_head; curr; curr = curr->next) {
+		if (curr->is_free && curr->size >= size)
+			return curr;
+	}
+	return NULL;
+}
---
+++ utils.h
@@ -0,0 +1,52 @@
+#include <pthread.h>		// pthread_mutex_unlock,pthread_mutex_lock,pthread_mutex_t
+#include <stddef.h>		// size_t
+#include <unistd.h>		// sbrk
+
+typedef void *void_ptr_t;
+typedef unsigned char byte_t;
+typedef unsigned int uint_t;
+
+// this struct is already memory-aligned for x86-64 processors
+typedef struct header_t {
+	size_t size;
+	byte_t is_free;		// status of mem block
+	struct header_t *next;
+} header_info_t;
+
+// prototypes
+header_info_t *get_free_block(size_t size);
+void_ptr_t malloc(size_t size);
+void_ptr_t calloc(size_t num, size_t nsize);
+void_ptr_t realloc(void *block, size_t size);
+void free(void_ptr_t block);
+void_ptr_t mm_mem_set(void_ptr_t src, int c, size_t n);
+void_ptr_t mm_mem_copy(void_ptr_t s1, void_ptr_t s2, size_t n);
+
+void_ptr_t mm_mem_set(void_ptr_t src, int c, size_t n)
+{
+	byte_t *tmp_src = (byte_t *) src;
+
+	int i;
+	for (i = 0; i != n; tmp_src[i++] = c) ;
+
+	return src;
+}
+
+void_ptr_t mm_mem_copy(void_ptr_t s1, void_ptr_t s2, size_t n)
+{
+	byte_t *c_dest = NULL, *c_src = NULL;
+	uint_t *l_dest = (uint_t *) s1, *l_src = (uint_t *) s2;
+
+	// first copy over data of 4-byte chunks
+	for (; n >= sizeof(uint_t); n -= sizeof(uint_t)) {
+		*l_dest++ = *l_src++;
+	}
+
+	// copy over the remainder data, 1-byte at a time
+	c_dest = (byte_t *) l_dest, c_src = (byte_t *) l_src;
+	while (n) {
+		*c_dest++ = *c_src, n--;
+	}
+
+	return s1;
+}
